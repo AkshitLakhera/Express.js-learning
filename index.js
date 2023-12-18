8556cfd01619c8049795d86a708d8ea6887dcf94
“[Akshit <akshitlakhera14@gmail.com>
2023-12-18 16:01:52 +0530

    All the code of hospital backen added

--- index.js
+++ index.js
@@ -3,25 +3,105 @@
 // / is a route after 3000/ everthing come here is route
 
 
-const express = require('express');
+// const express = require('express');
+// const app = express();
+
+// function sum(n) {
+//     let ans = 0;
+//     for (let i = 1; i <= n; i++) {
+//         ans = ans + i;
+//     }
+//     return ans;
+// }
+
+// app.get("/", function (req, res) {
+//     const n = req.query.n;
+//     const ans = sum(n);
+    
+//     // Use template string to concatenate the string and the answer
+//     res.send(`Hi, your answer is ${ans}`);
+// });
+
+// app.listen(3000, () => {
+//     console.log('Server is running at http://localhost:3000');
+// });
+
+const express = require("express");
 const app = express();
 
-function sum(n) {
-    let ans = 0;
-    for (let i = 1; i <= n; i++) {
-        ans = ans + i;
+const users = [{
+    name: "John",
+    kidneys: [{
+        healthy: false
+    }]
+}];
+
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    const johnKidneys = users[0].kidneys;
+    const numberOfKidneys = johnKidneys.length;
+    let numberOfHealthyKidneys = 0;
+    for (let i = 0; i<johnKidneys.length; i++) {
+        if (johnKidneys[i].healthy) {
+            numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
+        }
     }
-    return ans;
-}
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys
+    })
+})
 
-app.get("/", function (req, res) {
-    const n = req.query.n;
-    const ans = sum(n);
+app.post("/", function(req, res) {
     
-    // Use template string to concatenate the string and the answer
-    res.send(`Hi, your answer is ${ans}`);
-});
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    })
+    res.json({
+        msg: "Done!"
+    })
+})
 
-app.listen(3000, () => {
-    console.log('Server is running at http://localhost:3000');
-});
+// 411
+app.put("/", function(req, res) {
+    for (let i = 0; i<users[0].kidneys.length; i++) {
+        users[0].kidneys[i].healthy = true;
+    }
+    res.json({});
+})
+
+// removing all the unhealhty kidneys
+app.delete("/", function(req, res) {
+    if(isThereAtleastOneUnhealthyKidney()) {
+        const newKidneys = [];
+        for (let i = 0; i<users[0].kidneys.length; i++) {
+            if (users[0].kidneys[i].healthy) {
+                newKidneys.push({
+                    healthy: true
+                })
+            }
+        }
+        users[0].kidneys = newKidneys;
+        res.json({msg: "done"})   
+    } else {
+        res.status(411).json({
+            msg: "You have no bad kidneys"
+        });
+    }
+})
+
+function isThereAtleastOneUnhealthyKidney() {
+    let atleastOneUnhealthyKidney = false;
+    for (let i = 0; i<users[0].kidneys.length; i++) {
+        if (!users[0].kidneys[i].healthy) {
+            atleastOneUnhealthyKidney = true;
+        }
+    }
+    return atleastOneUnhealthyKidney
+}
+app.listen(3000);
+
